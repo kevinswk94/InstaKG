4906e6e5a7f0ff01204868b48ed762a4432620c2
EricsG95 <EricsG95@Gmail.com>
2015-06-10 17:26:58 +0800

    Parameterized Query is used for data retrieval

--- InstaKG/Profile.aspx.cs
+++ InstaKG/Profile.aspx.cs
@@ -7,7 +7,7 @@ namespace InstaKG
 {
     public partial class Profile1 : System.Web.UI.Page
     {
-        private string con = ConfigurationManager.ConnectionStrings["InstaKGConnectionString"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
@@ -19,13 +19,44 @@ namespace InstaKG
 
         private DataTable FetchAllImagesInfo()
         {
-            string accId= "2";
-            string sql = "Select * from Image where accountID=" + accId;
-            //sqlCmd.Parameters.AddWithValue("@ID", accountID.ToString());
-            SqlDataAdapter da = new SqlDataAdapter(sql, con);
+            string constr = ConfigurationManager.ConnectionStrings["InstaKGConnectionString"].ConnectionString;
+
             DataTable dt = new DataTable();
-            da.Fill(dt);
-            return dt;
+            SqlConnection con = new SqlConnection(constr);
+            SqlDataAdapter sda = new SqlDataAdapter();
+
+            //Dummy data of a particular user id
+            string accId= "1";
+            string strQuery = "Select * from Image where accountID= @ID";
+            
+            SqlCommand cmd = new SqlCommand(strQuery);
+            cmd.Parameters.AddWithValue("@ID", accId.Trim());
+            //("@ID", accId.Text.Trim()
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+
+            try
+            {
+                con.Open();
+                sda.SelectCommand = cmd;
+                sda.Fill(dt);
+                return dt;
+            }
+
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+                return null;
+            }
+
+            ////Backup of the basic
+            ////string accId= "1";
+            //string sql = "Select * from Image where accountID= @ID";
+            ////sqlCmd.Parameters.AddWithValue("@ID", accountID.ToString());
+            //SqlDataAdapter da = new SqlDataAdapter(sql, con);
+            //DataTable dt = new DataTable();
+            //da.Fill(dt);
+            //return dt;
         }
     }
 }
