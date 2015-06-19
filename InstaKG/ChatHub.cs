1d965c7b36d09864792f530256e6f6e5196c5d63
EricsG95 <EricsG95@Gmail.com>
2015-06-19 21:29:20 +0800

    Basic Chat System

---
+++ InstaKG/ChatHub.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNet.SignalR;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InstaKG
+{
+    public class ChatHub : Hub
+    {
+        #region Data Members
+
+        private static List<UserDetail> ConnectedUsers = new List<UserDetail>();
+        private static List<MessageDetail> CurrentMessage = new List<MessageDetail>();
+
+        #endregion Data Members
+
+        #region Methods
+
+        public void Connect(string userName)
+        {
+            var id = Context.ConnectionId;
+
+            if (ConnectedUsers.Count(x => x.ConnectionId == id) == 0)
+            {
+                ConnectedUsers.Add(new UserDetail { ConnectionId = id, UserName = userName });
+
+                // send to caller
+                Clients.Caller.onConnected(id, userName, ConnectedUsers, CurrentMessage);
+
+                // send to all except caller client
+                Clients.AllExcept(id).onNewUserConnected(id, userName);
+            }
+        }
+
+        public void SendMessageToAll(string userName, string message)
+        {
+            // store last 100 messages in cache
+            AddMessageinCache(userName, message);
+
+            // Broad cast message
+            Clients.All.messageReceived(userName, message);
+        }
+
+        public void SendPrivateMessage(string toUserId, string message)
+        {
+            string fromUserId = Context.ConnectionId;
+
+            var toUser = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == toUserId);
+            var fromUser = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == fromUserId);
+
+            if (toUser != null && fromUser != null)
+            {
+                // send to
+                Clients.Client(toUserId).sendPrivateMessage(fromUserId, fromUser.UserName, message);
+
+                // send to caller user
+                Clients.Caller.sendPrivateMessage(toUserId, fromUser.UserName, message);
+            }
+        }
+
+        // Fix it, no checking whether it is terminated gracefully or on force
+        // stopCalled is undefined
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            var item = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+            if (item != null)
+            {
+                ConnectedUsers.Remove(item);
+
+                var id = Context.ConnectionId;
+                Clients.All.onUserDisconnected(id, item.UserName);
+            }
+
+            return base.OnDisconnected(stopCalled);
+        }
+
+        #endregion Methods
+
+        #region private Messages
+
+        private void AddMessageinCache(string userName, string message)
+        {
+            CurrentMessage.Add(new MessageDetail { UserName = userName, Message = message });
+
+            if (CurrentMessage.Count > 100)
+                CurrentMessage.RemoveAt(0);
+        }
+
+        #endregion private Messages
+    }
+}
---
+++ InstaKG/Common/MessageDetail.cs
@@ -0,0 +1,9 @@
+﻿namespace InstaKG
+{
+    internal class MessageDetail
+    {
+        public string UserName { get; set; }
+
+        public string Message { get; set; }
+    }
+}
