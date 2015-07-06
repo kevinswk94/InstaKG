22243332db023b714b38f6faa2250c245dc88a1a
kevinswk94 <kevinswk94@gmail.com>
2015-07-06 16:13:01 +0800

    Removed ImageViewer.aspx and created BrowseImages.aspx

---
+++ InstaKG/BrowseImages.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/InstaKG.Master" AutoEventWireup="true" CodeBehind="BrowseImages.aspx.cs" Inherits="InstaKG.BrowseImages" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    
+</asp:Content>
--- InstaKG/BrowseImages.aspx.cs
+++ InstaKG/BrowseImages.aspx.cs
@@ -7,12 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace InstaKG
 {
-    public partial class ImageViewer : System.Web.UI.Page
+    public partial class BrowseImages : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string imageUrl = "~/ImageViewerHandler.ashx?id=" + ddl_imageTitle.SelectedValue.ToString();
-            img_selectedImage.ImageUrl = Page.ResolveUrl(imageUrl);
+
         }
     }
 }
