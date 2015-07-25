998e3cea75927fece00051dc895b40d87272fb96
kevinswk94 <kevinswk94@gmail.com>
2015-07-25 23:52:58 +0800

    Added encode and decode functions

---
+++ InstaKG/Steg_Decode.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/InstaKG.Master" AutoEventWireup="true" CodeBehind="Steg_Decode.aspx.cs" Inherits="InstaKG.Steg_Decode" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <script type="text/javascript">
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    $('#file').attr('src', e.target.result);
+                    $('#file').css({"visibility" : "visible"});
+                }
+                reader.readAsDataURL(input.files[0]);
+            } 
+        }
+        $("#img").change(function () {
+            readURL(this);
+        });
+    </script>
+
+    <div class="container">
+        <div class="well">
+            <fieldset class="form-horizontal">
+                <div class="row">
+                    <legend class="col-lg-offset-2 col-lg-5">Decode hidden message in image:</legend>
+                </div>
+                <div class="form-group">
+                    <asp:Label ID="lbl_fileName" CssClass="col-lg-3 control-label" runat="server">Choose File:</asp:Label>
+                    <div class="col-lg-6">
+                        <asp:FileUpload ID="fu_Image" runat="server" accept="image/png" onchange="readURL(this)" /> <%--Only allows PNG image filetypes--%>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <asp:Label ID="lbl_message" CssClass="col-lg-3 control-label" runat="server">Message:</asp:Label>
+                    <div class="col-lg-6">
+                        <asp:TextBox ID="tb_message" CssClass="form-control" runat="server" TextMode="MultiLine" ReadOnly="True"></asp:TextBox>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <asp:Label ID="lbl_imagePreview" CssClass="col-lg-3 control-label" runat="server">Preview:</asp:Label>
+                    <div class="col-lg-6">
+                        <img id="file" src="#" class="img-responsive center-block" style="max-width:400px;max-height:400px;" onerror="this.style.visibility='hidden'" />
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-lg-4 col-lg-offset-7">
+                        <asp:Button ID="btn_cancel" CssClass="btn btn-default" Text="Cancel" runat="server" OnClick="btn_cancel_Click" CausesValidation="false" />
+                        <asp:Button ID="btn_submit" runat="server" OnClick="btn_submit_Click" Text="Submit" CssClass="btn btn-primary" />
+                    </div>
+                </div>
+            </fieldset>
+        </div>
+    </div>
+</asp:Content>
---
+++ InstaKG/Steg_Encode.aspx.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace InstaKG
+{
+    public partial class Steg_Encode : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_submit_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Bitmap bmpImage = new System.Drawing.Bitmap(fu_Image.PostedFile.InputStream);
+            string message = tb_message.Text;
+
+            Bitmap stegoedImage = embedText(bmpImage, message);
+            MemoryStream bitmapStream = new MemoryStream();
+            stegoedImage.Save(bitmapStream, ImageFormat.Png);
+
+            byte[] bitmapBytes = bitmapStream.ToArray();
+
+            Response.Clear();
+            Response.Buffer = true;
+            Response.Charset = "";
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.ContentType = "image/png";
+            Response.AppendHeader("Content-Disposition", "attachment; filename=" + Path.GetFileNameWithoutExtension(fu_Image.FileName) + ".png");
+            Response.BinaryWrite(bitmapBytes);
+            Response.Flush();
+            Response.End();
+        }
+
+        protected void btn_cancel_Click(object sender, EventArgs e)
+        {
+            fu_Image.Attributes.Clear();
+            tb_message.Text = String.Empty;
+        }
+
+        public enum State
+        {
+            Hiding,
+            Filling_With_Zeros
+        };
+
+        private static Bitmap embedText(Bitmap bmp, string text)
+        {
+            // initially, we'll be hiding characters in the image
+            State state = State.Hiding;
+
+            // holds the index of the character that is being hidden
+            int charIndex = 0;
+
+            // holds the value of the character converted to integer
+            int charValue = 0;
+
+            // holds the index of the color element (R or G or B) that is currently being processed
+            long pixelElementIndex = 0;
+
+            // holds the number of trailing zeros that have been added when finishing the process
+            int zeros = 0;
+
+            // hold pixel elements
+            int R = 0, G = 0, B = 0;
+
+            // pass through the rows
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                // pass through each row
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    // holds the pixel that is currently being processed
+                    Color pixel = bmp.GetPixel(j, i);
+
+                    // now, clear the least significant bit (LSB) from each pixel element
+                    R = pixel.R - pixel.R % 2;
+                    G = pixel.G - pixel.G % 2;
+                    B = pixel.B - pixel.B % 2;
+
+                    // for each pixel, pass through its elements (RGB)
+                    for (int n = 0; n < 3; n++)
+                    {
+                        // check if new 8 bits has been processed
+                        if (pixelElementIndex % 8 == 0)
+                        {
+                            // check if the whole process has finished
+                            // we can say that it's finished when 8 zeros are added
+                            if (state == State.Filling_With_Zeros && zeros == 8)
+                            {
+                                // apply the last pixel on the image
+                                // even if only a part of its elements have been affected
+                                if ((pixelElementIndex - 1) % 3 < 2)
+                                {
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+
+                                // return the bitmap with the text hidden in
+                                return bmp;
+                            }
+
+                            // check if all characters has been hidden
+                            if (charIndex >= text.Length)
+                            {
+                                // start adding zeros to mark the end of the text
+                                state = State.Filling_With_Zeros;
+                            }
+                            else
+                            {
+                                // move to the next character and process again
+                                charValue = text[charIndex++];
+                            }
+                        }
+
+                        // check which pixel element has the turn to hide a bit in its LSB
+                        switch (pixelElementIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        // the rightmost bit in the character will be (charValue % 2)
+                                        // to put this value instead of the LSB of the pixel element
+                                        // just add it to it
+                                        // recall that the LSB of the pixel element had been cleared
+                                        // before this operation
+                                        R += charValue % 2;
+
+                                        // removes the added rightmost bit of the character
+                                        // such that next time we can reach the next one
+                                        charValue /= 2;
+                                    }
+                                } break;
+                            case 1:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        G += charValue % 2;
+
+                                        charValue /= 2;
+                                    }
+                                } break;
+                            case 2:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        B += charValue % 2;
+
+                                        charValue /= 2;
+                                    }
+
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                } break;
+                        }
+
+                        pixelElementIndex++;
+
+                        if (state == State.Filling_With_Zeros)
+                        {
+                            // increment the value of zeros until it is 8
+                            zeros++;
+                        }
+                    }
+                }
+            }
+            return bmp;
+        }
+    }
+}
