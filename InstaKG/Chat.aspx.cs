835a46f89d98f664bedfd2a8c25ed5b970ae7c67
EricsG95 <EricsG95@Gmail.com>
2015-08-08 22:44:48 +0800

    Fixed jumbled up message sequence UI presentation
    Praise the Lord! Thank you for waiting "/'=/'+WaiKit"

--- InstaKG/Chat.aspx.cs
+++ InstaKG/Chat.aspx.cs
@@ -1,53 +1,31 @@
 ﻿using System;
+using System.Text;
 using System.Web.Script.Serialization;
 using System.Web.Services;
 using System.Web.UI;
 
 namespace InstaKG
 {        
-    public partial class Chat : System.Web.UI.Page,IPostBackEventHandler
+    public partial class Chat : System.Web.UI.Page
     {
 
-        public static string  decryptedMessage = "too early execution";
-        public int privateCounter = 0;
+        public static string decryptedMessage = null;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+ 
             
-        }
-
-        protected string getDecryptedMessage()
-        {
-            privateCounter++;
-            // you can do more processing here
-            //bool privateChat = ChatHub.inPrivate;
-            //bool privateChat = true;
-
-            JavaScriptSerializer jSerializer;
-
-            decryptedMessage = refreshingDecryption();
-            
-            jSerializer = new JavaScriptSerializer();
-            // processes in decrypting
-            return jSerializer.Serialize(decryptedMessage);
-
-            // use serializer class which provides serialization and deserialization functionality for AJAX-enabled applications.
-
-            // serialize your object and return a serialized string
         }
 
         [WebMethod]
-        public static string  refreshingDecryption()
+        public static string refreshingDecryption()
         {
 
-            decryptedMessage = ChatHub.decryptedOTR;
+            // Getting the decrypted value
+            decryptedMessage = ChatHub.GetDecryptedOTR();
             return decryptedMessage;
 
         }
 
-
-        public void RaisePostBackEvent(string eventArgument)
-        {
-            decryptedMessage = ChatHub.decryptedOTR;
-        }
     }
 }
